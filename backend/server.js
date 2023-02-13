ed2764b7cbc9dede8b8b379a15d0fb1439f1320a
Seung taek Oh <elqkddl@naver.com>
2023-02-13 12:35:47 +0900

    learn. CSS 스타일 분기 - JS를 사용해 루트 요소의 class 속성 값 변경

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,18 @@ let params = {
   port: 3000,
   root: DIR.public,
   file: 'index.html',
-  mount: [['/scripts', DIR.src]],
+  mount: [['/scripts', DIR.src]], // 경로, rout
   open: false,
   wait: 200,
 };
 
 const customParams = parseArgv(argv);
+
+
+// 이 구문의 정체는 ?
+// 스프레드 오퍼레이터
+// 객체 합성 , 구조를 전개해서 객체를 합성할떄 사용
+// Object.assing() 
 
 params = { ...params, ...customParams };
 
